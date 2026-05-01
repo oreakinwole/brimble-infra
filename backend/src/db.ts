bac69e019f41841a57cbc2f9a5b2e27b0ba8dcc7
oreakinwole <oreakinwole@gmail.com>
2026-05-01 13:00:39 +0100

    remove loggin query

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,7 +3,3 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient({
   log: [{ emit: "event", level: "query" }],
 });
-
-prisma.$on("query", (e) => {
-  console.log(`[prisma] ${e.duration}ms ${e.query}`);
-});
